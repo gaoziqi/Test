5b8e0c96167187fffb9bfebbb26d0c06f05730ea
gaoziqi <571051761@qq.com>
2017-01-09 17:03:41 +0800

    64位更新treeview载入xml功能

--- InstConnection_X64/InstConnection/Form2.Designer.cs
+++ InstConnection_X64/InstConnection/Form2.Designer.cs
@@ -28,29 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.TreeNode treeNode14 = new System.Windows.Forms.TreeNode("测试连接图");
-            System.Windows.Forms.TreeNode treeNode15 = new System.Windows.Forms.TreeNode("互调抑制比测试连接图");
-            System.Windows.Forms.TreeNode treeNode16 = new System.Windows.Forms.TreeNode("无线电罗盘原理图", new System.Windows.Forms.TreeNode[] {
-            treeNode14,
-            treeNode15});
-            System.Windows.Forms.TreeNode treeNode17 = new System.Windows.Forms.TreeNode("塔康系统测量框图");
-            System.Windows.Forms.TreeNode treeNode18 = new System.Windows.Forms.TreeNode("发射峰脉冲值功率测量框图");
-            System.Windows.Forms.TreeNode treeNode19 = new System.Windows.Forms.TreeNode("射频脉冲频谱测量框图");
-            System.Windows.Forms.TreeNode treeNode20 = new System.Windows.Forms.TreeNode("同波道抑制性能测量框图");
-            System.Windows.Forms.TreeNode treeNode21 = new System.Windows.Forms.TreeNode("塔康机载设备原理图", new System.Windows.Forms.TreeNode[] {
-            treeNode17,
-            treeNode18,
-            treeNode19,
-            treeNode20});
-            System.Windows.Forms.TreeNode treeNode22 = new System.Windows.Forms.TreeNode("测试连接图");
-            System.Windows.Forms.TreeNode treeNode23 = new System.Windows.Forms.TreeNode("抗交叉调制干扰测试连接图");
-            System.Windows.Forms.TreeNode treeNode24 = new System.Windows.Forms.TreeNode("带内干扰测试连接图");
-            System.Windows.Forms.TreeNode treeNode25 = new System.Windows.Forms.TreeNode("带外干扰测试连接图");
-            System.Windows.Forms.TreeNode treeNode26 = new System.Windows.Forms.TreeNode("组合接收设备原理图", new System.Windows.Forms.TreeNode[] {
-            treeNode22,
-            treeNode23,
-            treeNode24,
-            treeNode25});
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -63,8 +40,8 @@
             // 
             // label1
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.Font = new System.Drawing.Font("黑体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label1.Location = new System.Drawing.Point(151, 9);
             this.label1.Name = "label1";
@@ -75,8 +52,8 @@
             // 
             // label3
             // 
-            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label3.Location = new System.Drawing.Point(245, 69);
             this.label3.Name = "label3";
@@ -97,51 +74,19 @@
             // 
             // treeView1
             // 
-            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
             this.treeView1.Location = new System.Drawing.Point(12, 104);
             this.treeView1.Name = "treeView1";
-            treeNode14.Name = "节点0";
-            treeNode14.Text = "测试连接图";
-            treeNode15.Name = "节点1";
-            treeNode15.Text = "互调抑制比测试连接图";
-            treeNode16.Name = "节点0";
-            treeNode16.Text = "无线电罗盘原理图";
-            treeNode17.Name = "节点0";
-            treeNode17.Text = "塔康系统测量框图";
-            treeNode18.Name = "节点1";
-            treeNode18.Text = "发射峰脉冲值功率测量框图";
-            treeNode19.Name = "节点2";
-            treeNode19.Text = "射频脉冲频谱测量框图";
-            treeNode20.Name = "节点3";
-            treeNode20.Text = "同波道抑制性能测量框图";
-            treeNode21.Name = "节点1";
-            treeNode21.Text = "塔康机载设备原理图";
-            treeNode22.Name = "节点0";
-            treeNode22.Text = "测试连接图";
-            treeNode23.Name = "节点1";
-            treeNode23.Text = "抗交叉调制干扰测试连接图";
-            treeNode24.Name = "节点2";
-            treeNode24.Text = "带内干扰测试连接图";
-            treeNode25.Name = "节点3";
-            treeNode25.Text = "带外干扰测试连接图";
-            treeNode26.Name = "节点2";
-            treeNode26.Text = "组合接收设备原理图";
-            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode16,
-            treeNode21,
-            treeNode26});
             this.treeView1.Size = new System.Drawing.Size(229, 409);
             this.treeView1.TabIndex = 21;
             this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
-            this.treeView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.treeView1_MouseClick);
-            this.treeView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.treeView1_MouseDown);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.pictureBox1.Location = new System.Drawing.Point(247, 104);
             this.pictureBox1.Name = "pictureBox1";
@@ -162,8 +107,8 @@
             // 
             // textBox1
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox1.Location = new System.Drawing.Point(249, 406);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
@@ -185,7 +130,6 @@
             this.Controls.Add(this.label1);
             this.Name = "Form2";
             this.Text = "教学模式";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_FormClosing);
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form2_FormClosed);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
--- InstConnection_X64/InstConnection/Form3.Designer.cs
+++ InstConnection_X64/InstConnection/Form3.Designer.cs
@@ -28,17 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("原理图1");
-            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("原理图2");
-            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("原理图2");
-            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("原理图2");
-            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("科目一", new System.Windows.Forms.TreeNode[] {
-            treeNode8,
-            treeNode9,
-            treeNode10,
-            treeNode11});
-            System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("科目二");
-            System.Windows.Forms.TreeNode treeNode14 = new System.Windows.Forms.TreeNode("科目三");
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.treeView1 = new System.Windows.Forms.TreeView();
@@ -48,8 +37,8 @@
             // 
             // label1
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.Font = new System.Drawing.Font("黑体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label1.Location = new System.Drawing.Point(151, 9);
             this.label1.Name = "label1";
@@ -70,28 +59,10 @@
             // 
             // treeView1
             // 
-            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
             this.treeView1.Location = new System.Drawing.Point(12, 106);
             this.treeView1.Name = "treeView1";
-            treeNode8.Name = "节点3";
-            treeNode8.Text = "原理图1";
-            treeNode9.Name = "节点7";
-            treeNode9.Text = "原理图2";
-            treeNode10.Name = "节点8";
-            treeNode10.Text = "原理图2";
-            treeNode11.Name = "节点9";
-            treeNode11.Text = "原理图2";
-            treeNode12.Name = "节点0";
-            treeNode12.Text = "科目一";
-            treeNode13.Name = "节点1";
-            treeNode13.Text = "科目二";
-            treeNode14.Name = "节点2";
-            treeNode14.Text = "科目三";
-            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode12,
-            treeNode13,
-            treeNode14});
             this.treeView1.Size = new System.Drawing.Size(229, 362);
             this.treeView1.TabIndex = 24;
             // 
